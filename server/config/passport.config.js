ef30a0b7a9560751726c2dfdfe0d548a2647c0cc
amoralyn <lovelyn.tijesunimi-israel@andela.com>
2016-05-26 13:07:09 +0100

    [#119263005] Fix Social Login  - Remove focus from input fields for social login  - Change Facebook Auth Config in Server file.  - Change Facebook variables in passport-facebook.js file

--- server/config/passport.config.js
+++ server/config/passport.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   'FACEBOOK': {
-    clientID: process.env.FACEBOOK_ID || '213258518873900',
+    clientID: process.env.FACEBOOK_ID || '245699389128451',
     clientSecret: process.env.FACEBOOK_SECRET ||
-      '76f13fab15780ef80bffc4ec19f24ae1',
+      '1493998cc7816b1624bcbb7d1ee96331',
     callbackURL: process.env.FACEBOOK_CALLBACK_URL ||
       'http://localhost:3000/auth/facebook/callback',
     profileFields: ['id', 'name', 'displayName', 'photos', 'gender', 'email']
