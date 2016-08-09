f93091dacb834a102e7a8fcfd4f2fe12fb1e7a7e
Oduntan Oluwatobiloba Stephen <stephen.oduntan@andela.com>
2016-08-09 04:30:38 +0100

    [Finishes #126985775] Fix e2e configuration bug

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,22 +1,24 @@
-// An example configuration file.
+// An example configuration file
 // https://raw.github.com/angular/protractor/master/example/conf.js
 exports.config = {
   // The address of a running selenium server.
   seleniumServerJar: './node_modules/selenium-server-standalone-jar/' +
-      'jar/selenium-server-standalone-2.44.0.jar', 
+      'jar/selenium-server-standalone-2.44.0.jar',
   // Make use you check the version in the folder
   allScriptsTimeout: 11000,
   chromeDriver: './node_modules/chromedriver/bin/chromedriver',
-  //seleniumAddress: 'http://localhost:8000/wd/hub',
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
     'browserName': 'chrome'
   },
-
-  framework: 'jasmine',
+  seleniumPort: 4444,
+  specs: ['test/e2e/*.js'],
   // Options to be passed to Jasmine-node.
   jasmineNodeOpts: {
     showColors: true,
-    defaultTimeoutInterval: 30000
-  }
+    defaultTimeoutInterval: 30000,
+    isVerbose : true,
+    includeStackTrace : true
+  },
+  framework: 'jasmine'
 };
